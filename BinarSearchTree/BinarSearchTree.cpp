a42ea6b79bc76a49d64b41fe76155f3e52764adc
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-07-02 15:40:00 +0700

    menbuat int main

--- BinarSearchTree/BinarSearchTree.cpp
+++ BinarSearchTree/BinarSearchTree.cpp
@@ -115,3 +115,20 @@ public:
 		}
 	}
 };
+
+int main()
+{
+	BinaryTree obj; 
+	while (true)
+	{
+		cout << "\nmenu" << endl;
+		cout << "1.implement insert operation" << endl;
+		cout << "2.perform inorder traversal" << endl;
+		cout << "3.perform preorder traversal" << endl;
+		cout << "4.perfrom postorder traversal" << endl;
+		cout << "5.exit" << endl;
+		cout << "\nEnter your choice (1-5) : ";
+
+		char
+	}
+}
